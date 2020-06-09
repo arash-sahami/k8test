c164961f2b7aeeb97764dada47f54a4d843598d7
Arash <arash_sahami@hotmail.com>
2020-06-09 15:57:08 +0200

    Add files via upload
    First commit.

---
+++ Controllers/K8TestController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using k8s;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace K8Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class K8TestController : ControllerBase
+    {
+        private readonly ILogger<K8TestController> _logger;
+        private readonly IKubernetes _kubernetes;
+
+        public K8TestController(ILogger<K8TestController> logger, IKubernetes kubernetes)
+        {
+            _logger = logger;
+            _kubernetes = kubernetes;
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetPodStatus(string podDnsName, string podNamespace)
+        {
+            _logger.LogInformation($"Get pod status for {podDnsName}.");
+
+            k8s.Models.V1Pod v1Pod  = await _kubernetes.ReadNamespacedPodStatusAsync(podDnsName, podNamespace);
+            return new JsonResult(v1Pod);
+        }
+
+        [HttpDelete]
+        public async Task<JsonResult> DeletePod(string podDnsName, string podNamespace)
+        {
+            _logger.LogInformation($"Deleting pod status for {podDnsName}.");
+
+            k8s.Models.V1Pod v1Pod = await _kubernetes.DeleteNamespacedPodAsync(podDnsName, podNamespace);
+            return new JsonResult(v1Pod);
+        }
+    }
+}
---
+++ runme.bat
@@ -0,0 +1,5 @@
+kubectl delete deployment k8test
+kubectl delete serviceaccount abc
+kubectl delete role abc
+kubectl delete rolebinding abc
+kubectl create -f deployment.yml
